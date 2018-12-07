c2d1e7e146353374cfa3364a9713790798830490
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2018-12-07 17:55:27 +0100

    Fix user-password-required authentification to edit the files

--- studINGI/local/config.php
+++ studINGI/local/config.php
@@ -1,3 +1,9 @@
-<?php
+<?php if (!defined('PmWiki')) exit();
+
+$WikiTitle = "Wiki student INGI";
+
 $AuthUser['myself'] = crypt('myself');
 include_once("$FarmD/scripts/authuser.php");
+
+$DefaultPasswords['edit'] = 'id:myself';
+
